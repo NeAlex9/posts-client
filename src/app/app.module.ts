a9d3c9c6b80191a320cf48a26521768a9e31e154
NeAlex9 <68583405+NeAlex9@users.noreply.github.com>
2023-06-21 23:06:50 +0300

    Add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { PostComponent } from './pages/posts/components/post/post.component';
 import { PostCreationFormComponent } from './pages/posts/components/post-creation-form/post-creation-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from './components/header/header.component';
     PostsComponent,
     PostComponent,
     PostCreationFormComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

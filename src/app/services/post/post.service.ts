d22b4a6f5f228ddce0fe2f2cbbd848b540d890c1
NeAlex9 <68583405+NeAlex9@users.noreply.github.com>
2023-06-25 12:54:15 +0300

    Minor refactoring

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { Post } from './models/post.model';
 import { CreatePostCommand } from './models/create-post.model';
-import { Subject, catchError, tap, throwError } from 'rxjs';
+import { Subject, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -13,7 +13,6 @@ export class PostService {
 
   private limit: number = 15;
   postsSubject: Subject<Post[]> = new Subject<Post[]>();
-  error: Subject<Error> = new Subject<Error>();
 
   constructor(
     private http: HttpClient,
@@ -32,12 +31,6 @@ export class PostService {
   }
 
   createPost(command: CreatePostCommand): Observable<any> {
-    return this.http.post<any>(`${environment.apiUrl}/posts`, command)
-      .pipe(
-        catchError((error: Error) => {
-          this.error.next(error);
-          return throwError(() => error);
-        })
-      );
+    return this.http.post<any>(`${environment.apiUrl}/posts`, command);
   }
 }
